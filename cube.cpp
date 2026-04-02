7b736d3e30f13b0e96e80943f5aba7058e91ad57
Logan Wyatt <loganwyatt1995@gmail.com>
2026-04-02 16:00:46 -0500

    Working on devising a naming system for each block

---
+++ cube.cpp
@@ -0,0 +1,19 @@
+#include "cube.h"
+
+namespace cube {
+
+    // class face {
+
+    // }; /* class face */
+
+    class cube {
+
+        private:
+
+        
+
+        public:
+
+    }; /* class cube */
+
+} /* namespace cube */
--- cube.h
+++ cube.h
@@ -3,14 +3,39 @@
 
 namespace cube {
 
-class cube {
+    enum cube_code {
+        // corner pieces
+        
+        // center edges
 
-private:
+        // center pieces
 
-public:
+    };
+    // enum color {
+    //     w = C8A2FF,
+    //     r = FFB7C5,
+    //     b = A7D8FF,
+    //     o = FFCBA4,
+    //     g = A8FFE3,
+    //     y = FFF3A3
+    // };
+
+    // class face {
+
+    //     private:
+
+    //     public:
+
+    // }; /* class face */
+
+    class cube {
+
+    private:
+
+    public:
 
 
-}; /* class cube */
+    }; /* class cube */
 
 } /* namesapce cube */
 
