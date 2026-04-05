071413fa5b5a2654082b71bccfd628f6738d2d9a
lrwyatt801 <loganwyatt1995@gmail.com>
2026-04-05 12:02:01 -0500

    Working on cube objects

--- cube.cpp
+++ cube.cpp
@@ -1,19 +1,45 @@
 #include "cube.h"
 
+using namespace cube;
+
 namespace cube {
 
-    // class face {
+    class block {
 
-    // }; /* class face */
+        private:
+        block_id id;
+        Vector3 block_position;
+
+        public:
+        block (block_id bid) : id(bid) {
+            
+        };
+    }; /* class block */
 
     class cube {
 
-        private:
+    block_id block_ids[26] = {
+        // corners
+        block_id::RWG, block_id::RGY, block_id::RYB, block_id::RBW,
+        block_id::OWG, block_id::OGY, block_id::OYB, block_id::OBW,
+        // egdes
+        block_id::RW, block_id::RG, block_id::RY, block_id::RB,
+        block_id::WG, block_id::GY, block_id::YB, block_id::BW,
+        block_id::OW, block_id::OG, block_id::OY, block_id::OB,
+        // centers
+        block_id::R, block_id::O, block_id::W, block_id::G, block_id::Y, block_id::B
+    };
 
-        
+    std::vector<block> blocks;
 
-        public:
+    public:
+    cube() {
+        for (block_id block_id : block_ids) {
+            blocks.push_back(block(block_id));
+        };
+
+    };
 
     }; /* class cube */
 
-} /* namespace cube */
+} /* namesapce cube */
--- cube.h
+++ cube.h
@@ -1,16 +1,25 @@
 #ifndef _CUBE_H
 #define _CUBE_H
 
+#include <cstdint>
+#include <raylib.h>
+#include <vector>
+
+
 namespace cube {
 
-    enum cube_code {
-        // corner pieces
-        
-        // center edges
-
-        // center pieces
-
+    enum class block_id : uint8_t {
+    NONE = 0,
+    // Bit mask for colors, that way we can easily combine them to get the color of each block
+    // 
+    R = 1, O = 1 << 1, W = 1 << 2, G = 1 << 3, Y = 1 << 4, B = 1 << 5,
+    RWG = R | W | G, RGY = R | G | Y, RYB = R | Y | B, RBW = R | B | W,
+    OWG = O | W | G, OGY = O | G | Y, OYB = O | Y | B, OBW = O | B | W,
+    RW = R | W, RG = R | G, RY = R | Y, RB = R | B,
+    WG = W | G, GY = G | Y, YB = Y | B, BW = B | W,
+    OW = O | W, OG = O | G, OY = O | Y, OB = O | B,
     };
+
     // enum color {
     //     w = C8A2FF,
     //     r = FFB7C5,
@@ -20,21 +29,18 @@ namespace cube {
     //     y = FFF3A3
     // };
 
-    // class face {
 
-    //     private:
+    class block
+    {
 
-    //     public:
+        public:
+        block ();
+    }; /* class block */
 
-    // }; /* class face */
-
-    class cube {
-
-    private:
-
+    class cube
+    {
     public:
-
-
+    cube();
     }; /* class cube */
 
 } /* namesapce cube */
